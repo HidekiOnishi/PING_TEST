4521805647b48c99ed033331ce6701f5e90d6185
Hideki Onishi <jin255@gmail.com>
2011-12-13 09:31:20 +0900

    second commit

---
+++ ping_test.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+######################################################
+#
+#				PING TEST Script
+#
+#				ping_test.pl
+#
+#				DENET.CO.,LTD
+#
+#		auther   : onishi@Midc 12/12/2011
+#		update by: onishi@Midc 12/12/2011
+#		usage    :./ping_test.pl <FileName>	
+#
+######################################################
+use strict;
+use warnings;
+
+if(@ARGV == 0){
+	print "Domain File Not Found.\n";
+	print "Usage: ping_test.pl <FileName>\n";
+	exit -1;
+}
+
+##Initialize section.##
+my $count = 0;
+my $domain_count = 0;
+my @ping = ( );
+open(IN,"< $ARGV[0]");
+open(OUT,"> ping.txt");
+
+while(<IN>){
+	@ping = `ping -c 1 ${_}`;
+	chomp $_;
+	$count = length($_);
+	print OUT $_;
+	while($count < 64){
+		print OUT "=";
+		$count++;
+	}
+	print OUT "\n";
+	print OUT $ping[1];
+	print OUT "================================================================\n";
+	print OUT "\n";
+	$domain_count++;
+}
+
+print OUT "Number of domains:".$domain_count."\n";
+
+close(IN);
+close(OUT);
+
+print "\n";
+print "            -----------------------\n";
+print "(*´ω｀*)< please check ping.txt! |\n";
+print "            -----------------------\n";
+print "\n";
+
+exit 0;
